5131036d1dc7bec81468f35b7f33805821a94d58
reddyin12 <gindra.kp86@gmail.com>
2019-04-16 16:53:54 -0700

    removed cookie policy from the app start up.

--- StocksWebApp/Startup.cs
+++ StocksWebApp/Startup.cs
@@ -32,12 +32,12 @@ namespace StocksWebApp
 
 			services.AddMvc();
 
-			services.Configure<CookiePolicyOptions>(options =>
-			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded = context => true;
-				options.MinimumSameSitePolicy = SameSiteMode.None;
-			});
+			//services.Configure<CookiePolicyOptions>(options =>
+			//{
+			//	// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+			//	options.CheckConsentNeeded = context => true;
+			//	options.MinimumSameSitePolicy = SameSiteMode.None;
+			//});
 
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
